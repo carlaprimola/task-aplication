af20893cfdefd772e9ec57b2c784c73bbbbd083b
Carla Escobar <carlaprimola@outlook.es>
2024-04-18 14:14:33 +0200

    last changes

--- client/README.md
+++
@@ -1,2 +0,0 @@
-Aplicación de Tareas hecha con React, Vite, Node y Express.
-Para la base de datos he usado MongoDB Atlass.
--- server/index.js
+++
@@ -1,8 +0,0 @@
-//ejecutar aplicacion
-import app from './app.js';
-import connectDB from './db.js';
-
-connectDB();
-app.listen(3000);
-console.log('✨ Servidor conectado', 3000);
-
